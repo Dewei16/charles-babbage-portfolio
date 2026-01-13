b319179e863e85dd959d209a45ff48b5b3ee2591
DARWIN.BESORIO16 <darwin.besorio16@gmail.com>
2026-01-13 19:27:10 +0800

    Added portfolio for hosting

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<Navbar />
+	<div class="container-fluid">
+		<About />
+		<Projects />
+		<Tools />
+		<Contacts />
+	</div>
+		<FooterComponent />
+</template>
+
+<script setup>
+	import Navbar from './components/Navbar.vue'
+	import About from './components/About.vue'
+	import Projects from './components/Projects.vue'
+	import Tools from './components/Tools.vue'
+	import Contacts from './components/Contacts.vue'
+	import FooterComponent from './components/FooterComponent.vue'
+</script>
